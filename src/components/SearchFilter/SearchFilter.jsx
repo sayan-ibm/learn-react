fe5ca7b7eddf413e7c8db1bf7961e3379561eda3
sayan-ibm <sayandas.bca@gmail.com>
2024-12-03 16:35:53 +0530

    3dec class work: counter & search filter

--- src/components/SearchFilter/SearchFilter.jsx
+++ src/components/SearchFilter/SearchFilter.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
-import "./SearchFilter.css"
 
 const SearchFilter = () => {
     const items = ["Banana", "Apple", "Orange", "Mango", "Pineapple", "Watermelon"];
     const [fruits, setFruits] = useState(items)
 
     function changeHandler(event){
-        let inpValue = event.target.value;
+        let inpValue = event.target.value.toLowerCase();
         if(inpValue === '') setFruits(items);
         console.log(event.target.value);
 
